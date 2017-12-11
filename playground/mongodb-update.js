b85a6bfab6060e2004a19f45d5e4884acc9252f6
Sayan Naskar <nascarsayan@gmail.com>
2017-12-11 14:44:39 +0530

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,37 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5a2e1f8267d9787eba8c8928')
+  // }, {
+  //   $set: {
+  //     completed: false
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then( (result) => {
+  //   console.log(JSON.stringify(result, undefined, 2));
+  // })
+
+  db.collection('Users').findOneAndUpdate({
+    // _id: new ObjectID('5a2e391a1b82ff5c86bb847e')
+    name: 'Chandra Sekhar Naskar'
+  }, {
+    $set: {
+      name: 'Sayan Naskar'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(JSON.stringify(result, undefined, 2));
+  })
+});
