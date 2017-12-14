ebece091707c498eddd927d4f1948f892905970e
Sayan Naskar <nascarsayan@gmail.com>
2017-12-14 16:14:51 +0530

    Add POST /users route and token method

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -5,6 +5,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
     return console.log('Unable to connect to MongoDB server');
   }
+  
   console.log('Connected to MongoDB server');
 
   // db.collection('Todos').findOneAndUpdate({
